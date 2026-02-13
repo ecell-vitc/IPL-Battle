8141ef5f6890229f5fd31654ec77326b09f8711b
ecell-vitc <helloecellvitc@gmail.com>
2026-02-13 23:57:15 +0530

    Revised import order

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -8,17 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 from .urls import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     )
